894dfdde82fc5ccb52ef9c7af52e98500d5ef0e8
ArifMiah07 <arifmiah.me322@gmail.com>
2024-11-17 03:36:17 +0600

    welcome back again|| practicing

---
+++ demo.js
@@ -0,0 +1,190 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+  console.log("Server is running");
+})
+
+app.listen(port, ()=>{
+  console.log(`server is running on port ${port}`);
+})
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// require('dotenv').config();
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// // Middleware
+
+// // app.use(
+// //   cors({
+// //       origin: [
+// //           // 'https://smart-kidz-95c4e.web.app', 
+// //           // 'https://smart-kidz-95c4e.firebaseapp.com',
+// //           'http://localhost:5173', 'http://localhost:5174'
+// //       ],
+// //   credentials: true
+// // }));
+
+// app.use(cors());
+// app.use(express.json());
+
+// // MongoDB connection URI
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fovwt3b.mongodb.net/south_asia?retryWrites=true&w=majority`;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect to the "south_asia" database and access its "southAsiaDB" collection
+//     await client.connect();
+//     const database = client.db("south_asia");
+//     const userCollection = database.collection("southAsiaDB");
+
+//     // GET route to fetch data from MongoDB and send it to the client
+//     app.get('/south-asia', async (req, res) => {
+//       try {
+//         const cursor = userCollection.find();
+//         const result = await cursor.toArray();
+//         res.json(result);
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+//         res.status(500).send('Error fetching data');
+//       }
+//     });
+
+//     console.log("Server connected to MongoDB and listening for requests.");
+//   } finally {
+//     // Ensure the client closes when finished
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
+// //////////////////////////
+
+
+
+/**
+ * 
+ * const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://user_name_is_error:User_Name_Is_Error0987654321@cluster0.fovwt3b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+      // Connect to the "insertDB" database and access its "userCollection" collection
+      const database = client.db("usersDB");
+      const userCollection = database.collection("users");
+
+    //get
+    app.get('/users', async(req, res)=>{
+      const cursor = userCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //single get
+    app.get('/users/:id', async (req, res)=>{
+      const id =req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const user = await userCollection.findOne(query);
+      res.send(user);
+
+    })
+
+    //post
+    app.post('/users', async (req, res) => {
+
+      const user = req.body;
+      console.log('new user', user);
+      // Insert the defined document into the "userCollection" collection
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+
+    });
+    //delete
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log('plz dlt from db', id);
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+//arifmiahme322
+//sg9HBDIhQ8OaCrx9
+// mongodb+srv://arifmiahme322:dHgZf5qYgUMaCYBM@cluster0.fovwt3b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//mongodb+srv://arifmiahme322:<password>@cluster0.fovwt3b.mongodb.net/
+// un=> sg9HBDIhQ8OaCrx9
+//ps=sg9HBDIhQ8OaCrx9
+
+
+app.get('/', (req, res)=>{
+    res.send('simple crud is ruuuunning');
+})
+
+app.listen(port, ()=>{
+    console.log(`simple crud is running on port: ${port}`);
+})
+ */
--- index.js
+++ index.js
@@ -1,88 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Middleware
-
-// app.use(
-//   cors({
-//       origin: [
-//           // 'https://smart-kidz-95c4e.web.app', 
-//           // 'https://smart-kidz-95c4e.firebaseapp.com',
-//           'http://localhost:5173', 'http://localhost:5174'
-//       ],
-//   credentials: true
-// }));
-
-app.use(cors());
-app.use(express.json());
-
-// MongoDB connection URI
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fovwt3b.mongodb.net/south_asia?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect to the "south_asia" database and access its "southAsiaDB" collection
-    await client.connect();
-    const database = client.db("south_asia");
-    const userCollection = database.collection("southAsiaDB");
-
-    // GET route to fetch data from MongoDB and send it to the client
-    app.get('/south-asia', async (req, res) => {
-      try {
-        const cursor = userCollection.find();
-        const result = await cursor.toArray();
-        res.json(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).send('Error fetching data');
-      }
-    });
-
-    console.log("Server connected to MongoDB and listening for requests.");
-  } finally {
-    // Ensure the client closes when finished
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
-
-
-
-/**
- * 
- * const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//middleware
+
+// middlewares
+
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
+
+// const cars = [
+//   {id: '1', name: 'c1', price: 201020 },
+//   {id: '2', name: 'c2', price: 203020 },
+//   {id: '3', name: 'c3', price: 203020 },
+//   {id: '4', name: 'c4', price: 204020 },
+//   {id: '5', name: 'c5', price: 205020 },
+// ]
+
+// NkmnjgC9tjqnZxEF
+// demotest
 
 
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const uri = "mongodb+srv://user_name_is_error:User_Name_Is_Error0987654321@cluster0.fovwt3b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://demotest:NkmnjgC9tjqnZxEF@cluster0.fovwt3b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -98,44 +38,39 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-      // Connect to the "insertDB" database and access its "userCollection" collection
-      const database = client.db("usersDB");
-      const userCollection = database.collection("users");
+    const carsCollection =  client.db('demoDb').collection('cars');
+    // const database = client.db("demoDb");
+    // const carsCollection = database.collection("cars");
 
-    //get
-    app.get('/users', async(req, res)=>{
-      const cursor = userCollection.find()
+    // crud operations
+
+    // get
+    
+    app.get('/cars', async (req, res)=>{
+      const cursor = carsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    });
-    //single get
-    app.get('/users/:id', async (req, res)=>{
-      const id =req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const user = await userCollection.findOne(query);
-      res.send(user);
-
     })
 
-    //post
-    app.post('/users', async (req, res) => {
-
-      const user = req.body;
-      console.log('new user', user);
-      // Insert the defined document into the "userCollection" collection
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-
-    });
-    //delete
-
-    app.delete('/users/:id', async(req, res)=>{
-      const id = req.params.id;
-      console.log('plz dlt from db', id);
-      const query = {_id: new ObjectId(id)}
-      const result = await userCollection.deleteOne(query);
+    // post
+    app.post('/cars', async(req, res)=>{
+      const newCars = req.body;
+      console.log(newCars);
+      const result = await carsCollection.insertOne(newCars);
       res.send(result);
     })
+
+    // app.post('/cars', async (req, res) => {
+    //   const newCar = req.body;
+    //   newCar.id = cars.length > 0 ? Math.max(...cars.map(c => c.id)) + 1 : 1;  // Ensure unique ID generation
+    //   cars.push(newCar);
+    //   // res.send(newCar);
+    //   console.log("new cars: " ,newCar);
+    //   const result = await carsCollection.insertOne(newCar);
+    //   res.send(result);
+    // })
+
+
 
 
     // Send a ping to confirm a successful connection
@@ -149,21 +84,21 @@ async function run() {
 run().catch(console.dir);
 
 
+// home routes
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 
 
-//arifmiahme322
-//sg9HBDIhQ8OaCrx9
-// mongodb+srv://arifmiahme322:dHgZf5qYgUMaCYBM@cluster0.fovwt3b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-//mongodb+srv://arifmiahme322:<password>@cluster0.fovwt3b.mongodb.net/
-// un=> sg9HBDIhQ8OaCrx9
-//ps=sg9HBDIhQ8OaCrx9
+// curd for cars
+// app.get('/cars', (req, res)=>{
+//   console.log("hi");
+//   // res.send("Hey take this car now");
+//   res.send(cars)
+// })
 
 
-app.get('/', (req, res)=>{
-    res.send('simple crud is ruuuunning');
-})
-
-app.listen(port, ()=>{
-    console.log(`simple crud is running on port: ${port}`);
-})
- */
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
