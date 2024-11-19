fb9b640498fd62bb3e860687f5448b038e9c6371
ArifMiah07 <arifmiah.me322@gmail.com>
2024-11-19 18:58:34 +0600

    hide secret keys

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 // demotest
 
 
-const uri = "mongodb+srv://demotest:NkmnjgC9tjqnZxEF@cluster0.fovwt3b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER_2}:${process.env.DB_PASS_2}@cluster0.fovwt3b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
